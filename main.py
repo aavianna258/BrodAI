8656a0412079300ab0db4a0bc31802613ca4ea0b
Artur Vianna <aavianna258@gmail.com>
2025-01-27 15:15:37 +0100

    added main to execute scripts from main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from scripts.publish_articles import create_blog_and_articles
+import json
+from src.packages.bloggers.shopify_blogger import ShopifyBlogger
 
-client_name = None
-response = create_blog_and_articles(client_name)
-print(response)
+client_name = "BB_HOUSE"
+blogger = ShopifyBlogger(client_name)
+response = blogger.list_blogs()
+print(json.dumps(response, indent=4))
